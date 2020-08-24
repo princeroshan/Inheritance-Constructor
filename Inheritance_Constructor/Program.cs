7bf5c7328c951a6d7142ae1fd63bbf6a1e1f2e03
Prince Roshan <princeroshan7@outlook.com>
2020-08-24 20:47:14 +0530

    Add project files.

---
+++ Inheritance_Constructor/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Constructor
+{
+    class Rectangle
+    {
+        double length;
+        double breadth;
+        public Rectangle()
+        {
+            Console.WriteLine("Default Constructor of Base Class");
+        }
+        public double Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+        public double Breadth
+        {
+            get { return breadth; }
+            set { breadth = value; }
+        }
+        public void DisplayDimen()
+        {
+            Console.WriteLine("Length :" + Length + " " + "Breadth :" + Breadth);
+        }
+    }
+
+    class AreaofRectangle : Rectangle
+    {
+        string shape;
+        public string Shape
+        {
+            get { return shape; }
+        }
+        public AreaofRectangle(string c, double l, double b)
+        {
+            shape = c;
+            Length = l;
+            Breadth = b;
+        }
+        public double Area()
+        {
+            return (Length * Breadth);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AreaofRectangle obj = new AreaofRectangle("Rectangle", 12.0, 9.0);
+            obj.DisplayDimen();
+            Console.WriteLine("Shape :" + obj.Shape);
+            double area = obj.Area();
+            Console.WriteLine("Area :" + area+"cm2");
+            Console.ReadKey();
+        }
+    }
+}
